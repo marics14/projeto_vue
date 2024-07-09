1cc114daa3008c1a9edc54108fad575903a1ddcd
marics14 <162575459+marics14@users.noreply.github.com>
2024-07-09 11:36:22 -0300

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="hello">
+    <body>
+  <header>
+
+    <div id="conteudo-principal">
+      <h1>Gatos</h1>
+
+      <p>
+        É sempre bom ter um amiguinho de estimação!
+
+      </p>
+      <div>
+        <img class="" src="gatosiames.webp" width="33%">
+      </div>
+
+      <div>
+        <img class="" src="gatoragdoll.webp" width="33%">
+      </div>
+
+      <div>
+        <img class="" src="gatolaranja.jpg" width="33%">
+      </div>
+
+
+    </div>
+
+
+    <iframe width="976" height="549" src="https://www.youtube.com/embed/LMD6MqwErzc"
+      title="Bebês Gatos   Compilação De Vídeos Fofos E Engraçados De Gatos   Gatinhos Fofos No Mundo" frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen></iframe>
+
+  </header>
+
+
+</body>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
